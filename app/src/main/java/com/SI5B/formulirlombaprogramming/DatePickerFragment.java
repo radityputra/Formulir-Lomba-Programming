b57acb4ce9789d4999ee939ccd90a4324f0c0775
Raditya Putra <radityaa.pr@gmail.com>
2022-10-19 13:56:08 +0700

    formulirlombaprogramming
    19/10/2022

---
+++ app/src/main/java/com/SI5B/formulirlombaprogramming/DatePickerFragment.java
@@ -0,0 +1,62 @@
+package com.SI5B.formulirlombaprogramming;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.widget.DatePicker;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import java.util.Calendar;
+
+public class DatePickerFragment extends DialogFragment
+implements DatePickerDialog.OnDateSetListener {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public static DatePickerFragment newInstance(String param1, String param2) {
+        DatePickerFragment fragment = new DatePickerFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year,
+                          int month, int day) {
+        ConfirmActivity CA = (ConfirmActivity) getActivity();
+        CA.processDatePickerResult(year, month, day);
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle
+                                         savedInstanceState) {
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+}
+
+
+
